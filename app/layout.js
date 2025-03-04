6026eeed10018e042f423f137eb0d0480584a2c1
Priyanandan-4 <priyanandanofficialwork@gmail.com>
2025-03-04 15:50:50 +0530

    ui:almost complete

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, } from "next/font/google";
+import 'lenis/dist/lenis.css';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.js
+++ app/page.js
@@ -1,101 +1,96 @@
+"use client"
+import { useEffect, useRef } from "react";
+import Lenis from 'lenis'
+import gsap from "gsap/all";
 import Image from "next/image";
+import logo from "../public/icons/logo.svg"
+import arrowdown from "../public/icons/arrowdown.svg"
+// import video from "../public/video/banner.mp4"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const heroref = useRef(null);
+  const textref = useRef(null);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    if (heroref.current) {
+      gsap.to(heroref.current, {
+        y: "-100%",
+        duration: 1,
+        delay: 0.5,
+        ease: "power2.inOut",
+      });
+    }
+
+
+    const tl = gsap.timeline();
+    tl.from(textref,{ y:40, ease:"power2.inOut"})
+
+
+
+
+
+
+
+  }, []);
+
+  return (
+    <div className="w-full">
+      <div className="w-full h-screen text-7xl font-inter font-[800] text-white flex justify-center items-center text-center leading-tight bg-[#eb5dd6]">
+        THE DESIGN<br />AGENCY
+      </div>
+      <div ref={heroref} className="w-full h-screen  bg-[#afee6b]  text-black flex flex-col justify-between items-center absolute tracking-widest">
+        <div className="pt-10"><Image src={logo} width={50} height={50} alt="logo" /></div>
+        <div className="text-center  text-6xl md:text-8xl lg:text-9xl font-inter font-[800]"><div ref={textref} >THE DESIG</div><div>AGENCY</div></div>
+        <div><Image src={arrowdown} width={60} height={60} alt="logo" /></div>
+      </div>
+
+
+
+
+
+      <div className="z-10 w-full h-96 md:h-screen bg-white flex justify-center items-center  text-center leading-tight   text-8xl  ">
+      
+      <video
+        src="/video/banner.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-[40%] h-[40%]"
+      />
+
+      </div> 
+     <div className="w-full h-96 md:h-screen  font-bold bg-[#9ce968]  text-black flex flex-col justify-center items-center">
+        <div className="pt-10 text-2xl">WHO WE ARE</div>
+        <div className="text-center  text-6xl md:text-8xl font-bold"> THE DESIGN<br />AGENCY</div>
+        <div className="text-center leading-tight text-20 md:text-2xl   pt-5 md:pt-8">Engaging user experience, world class<br/> web design & development, unique<br/> brand identity solutions to shine.</div>
+      </div>
+
+
+      <div className=" w-full h-80 md:h-96 flex justify-center items-center overflow-x-hidden space-x-4  bg-[#9ce968]">
+        
+        <div className="h-48 md:h-64 w-[250px] md:w-[450px] bg-amber-800 rounded-2xl flex-shrink-0"></div>
+        <div className="h-48 md:h-64 w-[250px] md:w-[450px] bg-amber-800 rounded-2xl flex-shrink-0"></div>
+        <div className="h-48 md:h-64 w-[250px] md:w-[450px] bg-amber-800 rounded-2xl flex-shrink-0"></div>
+        <div className="h-48 md:h-64 w-[250px] md:w-[450px] bg-amber-800 rounded-2xl flex-shrink-0"></div>
+        <div className="h-48 md:h-64 w-[250px] md:w-[450px] bg-amber-800 rounded-2xl flex-shrink-0"></div>
+      </div>
+
+
+
+
+
+      <div className="bg-[#d24ad0] w-full h-screen">
+
+      </div>
+    </div> 
   );
 }
